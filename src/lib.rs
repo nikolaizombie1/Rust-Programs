8d7761ab3bcd8ad9f65ea6176bd4dc006af56f34
nikolaizombie1 <fabio.matos999@gmail.com>
2022-07-12 18:03:15 -0400

    Implemented new_name method

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use walkdir::DirEntry as WDirentry;
 use std::path::Path;
 use std::fs;
 use std::io;
-pub fn create_sorted_file_enties(path: &Path) -> Vec<(WDirentry,String)> {
+pub fn create_sorted_file_entries(path: &Path) -> Vec<(WDirentry,String)> {
     let entries = WalkDir::new(path).max_depth(1).into_iter().filter_map(|file| file.ok());
     let mut files: Vec<WDirentry> = Vec::new();
     for file in entries {
@@ -20,8 +20,15 @@ pub fn create_sorted_file_enties(path: &Path) -> Vec<(WDirentry,String)> {
     fileentries
 }
 
-pub fn accept_and_validate_nw_name() -> String {
-    let mut retstring: String = String::new();
-    io::stdin().read_line(&mut retstring).expect("failed to read input");
-    retstring
+pub fn accept_and_validate_new_name() -> String {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer);
+    if buffer.contains('/') {
+        loop {
+            let mut buffer = String::new();
+            io::stdin().read_line(&mut buffer);
+            if !buffer.contains('/') {return buffer}
+        }
+    }
+    buffer
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ use std::env;
 use std::process;
 use plexformatter;
 fn main() {
-    let files = plexformatter::create_sorted_file_enties(env::current_dir().unwrap_or_else(|e| {
+    let files = plexformatter::create_sorted_file_entries(env::current_dir().unwrap_or_else(|e| {
         eprintln!("Error executing in current directory: {}",e);
         process::exit(1)
     }).as_path());
+    let x = plexformatter::accept_and_validate_new_name();
+    println!("{}",x)
 }
 
 
