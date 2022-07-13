c9e90f4088fe8be5611dbd659c6d36999f5171f3
nikolaizombie1 <fabio.matos999@gmail.com>
2022-07-13 10:37:43 -0400

    Added create int list and tests for it

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use natord;
+use std::collections::HashSet;
 use walkdir::WalkDir;
 use walkdir::DirEntry as WDirentry;
 use std::path::Path;
@@ -26,7 +27,7 @@ pub fn accept_and_validate_new_name() -> String {
     if buffer.contains('/') {
         loop {
             let mut buffer = String::new();
-            io::stdin().read_line(&mut buffer).expect("Error reading from ");
+            io::stdin().read_line(&mut buffer).expect("Error reading from stdin");
             if !buffer.contains('/') {return buffer}
         }
     }
@@ -44,4 +45,57 @@ pub fn accept_and_validate_range_string() -> Vec<String> {
         retvec.push(String::from(*range));
     }
     retvec
+}
+
+pub fn create_int_list(ranges: Vec<String>) -> Vec<usize> {
+    let rerange = regex::Regex::new(r"^\d+-\d+\d*$").unwrap();
+    let recsv = regex::Regex::new(r"^(\d,)+\d*$").unwrap();
+    let resingle = regex::Regex::new(r"^\d+$").unwrap();
+    let mut parsedrange: HashSet<usize> = HashSet::new();
+    for range in ranges {
+        if rerange.is_match(&range) {
+            let temprange: Vec<&str> = range.split('-').collect();
+            let begin = temprange.get(0).unwrap().parse::<usize>().unwrap();
+            let end = temprange.get(1).unwrap().parse::<usize>().unwrap();
+            for index in begin..end+1 {
+                parsedrange.insert(index);
+            }
+        }
+        else if recsv.is_match(&range) {
+            let temprange: Vec<&str> = range.split(',').collect();
+            for range in temprange {
+                parsedrange.insert(range.parse::<usize>().unwrap());
+            }
+        }
+        else if resingle.is_match(&range) {
+            parsedrange.insert(range.parse::<usize>().unwrap());
+        }
+    }
+    let mut parsedrange: Vec<usize> = parsedrange.into_iter().collect();
+    parsedrange.sort();
+    parsedrange
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn parses_range_correctly() {
+        let mut inputs: Vec<String> = vec![
+            String::from("webfhbewhfbh"),
+            String::from("1-5"),
+            String::from("ehjfwjbebw1-5jfenwjfe"),
+            String::from("rvebvhreb-h43hfh"),
+            String::from("_"),
+            String::from("6,7,8"),
+            String::from("rgejgrjheh,grehbrehgb,grehbhrbe"),
+            String::from(",,,,,,,,"),
+            String::from("---"),
+            String::from("1-1-1"),
+            String::from("1aas-b--c")
+            ];
+        let correctanswer: Vec<usize> = vec![1,2,3,4,5,6,7,8];
+        let answer = create_int_list(inputs);
+        assert_eq!(correctanswer,answer);
+    } 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use std::env;
-use std::process;
 use plexformatter;
 fn main() {
-    let x = plexformatter::accept_and_validate_range_string();
-    println!("{:?}",x)
+    println!("{:?}",plexformatter::create_int_list(vec![String::from("")]));
 }
 
 
