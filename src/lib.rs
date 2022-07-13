de023f1699ed194a3c7655a72966a738e2d7da27
nikolaizombie1 <fabio.matos999@gmail.com>
2022-07-13 09:06:23 -0400

    Implemented range string method

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@ use natord;
 use walkdir::WalkDir;
 use walkdir::DirEntry as WDirentry;
 use std::path::Path;
-use std::fs;
 use std::io;
+use regex;
 pub fn create_sorted_file_entries(path: &Path) -> Vec<(WDirentry,String)> {
     let entries = WalkDir::new(path).max_depth(1).into_iter().filter_map(|file| file.ok());
     let mut files: Vec<WDirentry> = Vec::new();
@@ -16,19 +16,32 @@ pub fn create_sorted_file_entries(path: &Path) -> Vec<(WDirentry,String)> {
     for file in files {
         fileentries.push((file.clone(),String::from(file.file_name().to_str().unwrap())));
     }
-    fileentries.sort_by(|a,b| natord::compare(&a.1, &b.1));
+    fileentries.sort_by(|a,b| natord::compare(&a.1.to_lowercase(), &b.1.to_lowercase()));
     fileentries
 }
 
 pub fn accept_and_validate_new_name() -> String {
     let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer);
+    io::stdin().read_line(&mut buffer).expect("Error reading from stdin");
     if buffer.contains('/') {
         loop {
             let mut buffer = String::new();
-            io::stdin().read_line(&mut buffer);
+            io::stdin().read_line(&mut buffer).expect("Error reading from ");
             if !buffer.contains('/') {return buffer}
         }
     }
     buffer
+}
+
+pub fn accept_and_validate_range_string() -> Vec<String> {
+    let re = regex::Regex::new(r"^\d+-\d+\d*$|^(\d,)+\d*$|^\d+$").unwrap();
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).expect("Error reading from ");
+    let ranges: Vec<&str> = buffer.split_ascii_whitespace().collect();
+    let ranges = ranges.iter().filter(|range| re.is_match(range));
+    let mut retvec: Vec<String> = Vec::new();
+    for range in ranges {
+        retvec.push(String::from(*range));
+    }
+    retvec
 }
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,8 @@ use std::env;
 use std::process;
 use plexformatter;
 fn main() {
-    let files = plexformatter::create_sorted_file_entries(env::current_dir().unwrap_or_else(|e| {
-        eprintln!("Error executing in current directory: {}",e);
-        process::exit(1)
-    }).as_path());
-    let x = plexformatter::accept_and_validate_new_name();
-    println!("{}",x)
+    let x = plexformatter::accept_and_validate_range_string();
+    println!("{:?}",x)
 }
 
 
