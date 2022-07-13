0be31f70a0a22aafbed301e58fe4be8dfc3d5b96
nikolaizombie1 <fabio.matos999@gmail.com>
2022-07-13 13:20:07 -0400

    Implemented ask for season and preview methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,76 @@
 use natord;
-use std::collections::HashSet;
-use std::vec;
-use walkdir::WalkDir;
-use walkdir::DirEntry as WDirentry;
-use std::path::Path;
-use std::io;
 use regex;
-pub fn create_sorted_file_entries(path: &Path) -> Vec<(WDirentry,String)> {
-    let entries = WalkDir::new(path).max_depth(1).into_iter().filter_map(|file| file.ok());
+use std::collections::HashSet;
+use std::io;
+use std::path::Path;
+use std::vec;
+use walkdir::DirEntry as WDirentry;
+use walkdir::WalkDir;
+pub fn create_sorted_file_entries(path: &Path) -> Vec<(WDirentry, String, String)> {
+    let entries = WalkDir::new(path)
+        .max_depth(1)
+        .into_iter()
+        .filter_map(|file| file.ok());
     let mut files: Vec<WDirentry> = Vec::new();
     for file in entries {
         if file.metadata().unwrap().is_file() {
             files.push(file);
         }
     }
-    let mut fileentries: Vec<(WDirentry,String)> = Vec::new();
+    let mut fileentries: Vec<(WDirentry, String, String)> = Vec::new();
     for file in files {
-        fileentries.push((file.clone(),String::from(file.file_name().to_str().unwrap())));
+        let extention = String::from(
+            file.file_name()
+                .to_str()
+                .unwrap()
+                .split('.')
+                .last()
+                .unwrap(),
+        );
+        fileentries.push((
+            file.clone(),
+            String::from(file.file_name().to_str().unwrap()),
+            extention,
+        ));
     }
     if fileentries.len() == 0 {
         eprintln!("Directory is empty");
         std::process::exit(1)
     }
-    fileentries.sort_by(|a,b| natord::compare(&a.1.to_lowercase(), &b.1.to_lowercase()));
+    fileentries.sort_by(|a, b| natord::compare(&a.1.to_lowercase(), &b.1.to_lowercase()));
     fileentries
 }
 
-pub fn accept_and_validate_new_name() -> String {
+pub fn accept_and_validate_new_name(name: String) -> String {
+    if !name.is_empty() && !name.contains('/') {
+        return name
+    }
     let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer).expect("Error reading from stdin");
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("Error reading from stdin");
     if buffer.contains('/') {
         loop {
             let mut buffer = String::new();
-            io::stdin().read_line(&mut buffer).expect("Error reading from stdin");
-            if !buffer.contains('/') {return buffer}
+            io::stdin()
+                .read_line(&mut buffer)
+                .expect("Error reading from stdin");
+            if !buffer.contains('/') {
+                return buffer;
+            }
         }
     }
     buffer
 }
 
-pub fn accept_and_validate_range_string(entries: Vec<(WDirentry,String)>) -> Vec<String> {
+pub fn accept_and_validate_range_string(entries: Vec<(WDirentry, String, String)>) -> Vec<String> {
     let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer).expect("Error reading from ");
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("Error reading from ");
     if buffer.chars().nth(0).unwrap() == '\n' {
-        let retstring = format!("1-{}",entries.len());
-        return vec![retstring]
+        let retstring = format!("1-{}", entries.len());
+        return vec![retstring];
     }
     let re = regex::Regex::new(r"^\d+-\d+\d*$|^(\d,)+\d*$|^\d+$").unwrap();
     let ranges: Vec<&str> = buffer.split_ascii_whitespace().collect();
@@ -66,17 +92,15 @@ pub fn create_int_list(ranges: Vec<String>) -> Vec<usize> {
             let temprange: Vec<&str> = range.split('-').collect();
             let begin = temprange.get(0).unwrap().parse::<usize>().unwrap();
             let end = temprange.get(1).unwrap().parse::<usize>().unwrap();
-            for index in begin..end+1 {
+            for index in begin..end + 1 {
                 parsedrange.insert(index);
             }
-        }
-        else if recsv.is_match(&range) {
+        } else if recsv.is_match(&range) {
             let temprange: Vec<&str> = range.split(',').collect();
             for range in temprange {
                 parsedrange.insert(range.parse::<usize>().unwrap());
             }
-        }
-        else if resingle.is_match(&range) {
+        } else if resingle.is_match(&range) {
             parsedrange.insert(range.parse::<usize>().unwrap());
         }
     }
@@ -85,8 +109,34 @@ pub fn create_int_list(ranges: Vec<String>) -> Vec<usize> {
     parsedrange
 }
 
-pub fn is_range_ok() -> bool {
-    false
+pub fn is_range_ok(len: usize, ranges: Vec<usize>) -> bool {
+    for range in ranges {
+        if range > len || range < 1 {
+            return false;
+        }
+    }
+    true
+}
+
+pub fn preview_changes(entries: Vec<(WDirentry, String, String)>,newname: String,season: usize) {
+    let entries_iter = entries.iter();
+    for (index,entry) in entries_iter.enumerate() {
+        println!("{}. {} ----> {newname} S{season}E{}.{}",index+1,entry.1,index+1,entry.2);
+    }
+}
+
+pub fn ask_for_season_and_validate() -> usize {
+    loop {
+        let mut season: String = String::new();
+        io::stdin()
+            .read_line(&mut season)
+            .expect("Error reading from ");
+        let season: usize = match season.trim().parse() {
+           Ok(num) => num,
+           Err(_) => continue, 
+        };
+        return season
+    }
 }
 
 #[cfg(test)]
@@ -105,14 +155,14 @@ mod tests {
             String::from(",,,,,,,,"),
             String::from("---"),
             String::from("1-1-1"),
-            String::from("1aas-b--c")
-            ];
-        let correctanswer: Vec<usize> = vec![1,2,3,4,5,6,7,8];
+            String::from("1aas-b--c"),
+        ];
+        let correctanswer: Vec<usize> = vec![1, 2, 3, 4, 5, 6, 7, 8];
         let answer = create_int_list(inputs);
-        assert_eq!(correctanswer,answer);
+        assert_eq!(correctanswer, answer);
     }
-    #[test]
-    fn verify_ranges_correctly() {
-        
-    } 
+    //#[test]
+    //fn verify_name() {
+    //    assert_eq!(String::from("Fate Extra\n"),accept_and_validate_new_name(String::from("")));
+    //}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
 use plexformatter;
-use std::env;
 fn main() {
-    let x = plexformatter::create_sorted_file_entries(env::current_dir().unwrap().as_path());
-    println!("{:?}",x);
-    let y = plexformatter::accept_and_validate_range_string(x);
-    println!("{:?}",y);
+    let x = plexformatter::create_sorted_file_entries(std::env::current_dir().unwrap().as_path());
+    plexformatter::preview_changes(x, String::from("Fate "), 1)
 }
-
-
